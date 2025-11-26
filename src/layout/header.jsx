bce4c89d69ca3340933e62230392d8af306069b8
XaydarovSardor <birbalobirbattar@gmail.com>
2025-11-26 15:51:41 +0500

    ddss

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -7,6 +7,7 @@ import API_URL from "../services/api"
 
 export const Header = () => {
     
+    
     const [openModal, setOpenModal] = useState(false)
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
